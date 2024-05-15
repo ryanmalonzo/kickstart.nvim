a3fba9c5e33aa92587d247e09e74fa74eef9c5bb
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2024-05-15 10:06:49 +0200

    Use catppuccin color theme + add trouble.nvim + add copilot

--- init.lua
+++ init.lua
@@ -792,6 +792,11 @@ require('lazy').setup({
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
+    config = function()
+      require('catppuccin').setup {
+        flavour = 'mocha',
+      }
+    end,
   },
 
   -- Highlight todo, notes, etc in comments
