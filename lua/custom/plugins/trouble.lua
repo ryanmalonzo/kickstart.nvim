f8f144fbe67adff8a5a1b6bdc9261ae8f3778e56
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2024-05-31 16:46:19 +0200

    Update trouble.nvim to version 3

--- lua/custom/plugins/trouble.lua
+++ lua/custom/plugins/trouble.lua
@@ -1,6 +1,7 @@
 return {
   'folke/trouble.nvim',
-  branch = 'dev', -- IMPORTANT!
+  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  cmd = 'Trouble',
   keys = {
     {
       '<leader>xx',
@@ -33,17 +34,4 @@ return {
       desc = 'Quickfix List (Trouble)',
     },
   },
-  opts = {
-    modes = {
-      test = {
-        mode = 'diagnostics',
-        preview = {
-          type = 'split',
-          relative = 'win',
-          position = 'right',
-          size = 0.3,
-        },
-      },
-    },
-  }, -- for default options, refer to the configuration section for custom setup.
 }
