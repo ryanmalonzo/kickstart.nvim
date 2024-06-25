4b831f87ed7c8f4352f7a9e3aa53194c097152be
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2024-06-25 12:06:55 +0200

    Add harpoon

---
+++ lua/custom/plugins/harpoon.lua
@@ -0,0 +1,36 @@
+return {
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    local harpoon = require 'harpoon'
+    harpoon:setup()
+
+    vim.keymap.set('n', '<leader>a', function()
+      harpoon:list():add()
+    end)
+    vim.keymap.set('n', '<leader>l', function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end)
+
+    vim.keymap.set('n', '<leader>&', function()
+      harpoon:list():select(1)
+    end)
+    vim.keymap.set('n', '<leader>é', function()
+      harpoon:list():select(2)
+    end)
+    vim.keymap.set('n', '<leader>"', function()
+      harpoon:list():select(3)
+    end)
+    vim.keymap.set('n', "<leader>'", function()
+      harpoon:list():select(4)
+    end)
+
+    vim.keymap.set('n', '<C-k>', function()
+      harpoon:list():prev()
+    end)
+    vim.keymap.set('n', '<C-j>', function()
+      harpoon:list():next()
+    end)
+  end,
+}
