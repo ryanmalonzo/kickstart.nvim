c85fe4486e5704a89da15d78c7f26b81b58d0383
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2024-06-27 16:31:08 +0200

    Use mini.surround

--- init.lua
+++ init.lua
@@ -750,7 +750,13 @@ require('lazy').setup({
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      -- require('mini.surround').setup()
+      require('mini.surround').setup {
+        mappings = {
+          add = 'za',
+          delete = 'zd',
+          replace = 'zr',
+        },
+      }
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
