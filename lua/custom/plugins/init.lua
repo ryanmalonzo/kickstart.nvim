5622643442fe0dfc1cca95387e4dc82bf7e7d288
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-05-01 09:08:54 +0200

    feat: add folke/snacks.nvim plugin configuration

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,20 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'folke/snacks.nvim',
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      lazygit = { enabled = true },
+    },
+    keys = {
+      {
+        '<leader>lg',
+        function()
+          Snacks.lazygit()
+        end,
+        desc = 'Lazygit',
+      },
+    },
+  },
+}
