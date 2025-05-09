9c821830b485429e80e4a7d529d1213ce6eb2574
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-05-09 08:10:33 +0200

    feat: configure copilot.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -31,7 +31,35 @@ return {
     cmd = 'Copilot',
     event = 'InsertEnter',
     config = function()
-      require('copilot').setup {}
+      require('copilot').setup {
+        suggestion = {
+          auto_trigger = true,
+          keymap = {
+            accept = '<C-a>',
+            next = '<C-n>',
+            prev = '<C-p>',
+            dismiss = '<C-d>',
+          },
+        },
+        filetypes = {
+          yaml = true,
+          markdown = true,
+        },
+      }
+
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'BlinkCmpMenuOpen',
+        callback = function()
+          vim.b.copilot_suggestion_hidden = true
+        end,
+      })
+
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'BlinkCmpMenuClose',
+        callback = function()
+          vim.b.copilot_suggestion_hidden = false
+        end,
+      })
     end,
   },
 }
